4a500d55a26a35a25f4d7251cf661ae5183685c4
samuel gomes <stoone.holy@gmail.com>
2021-01-24 13:29:54 -0300

    Fixed (?) error "Plugin already initialized".

--- src/main/java/net/stonegomes/commons/CommonsPlugin.java
+++ src/main/java/net/stonegomes/commons/CommonsPlugin.java
@@ -3,14 +3,43 @@ package net.stonegomes.commons;
 import net.stonegomes.commons.module.ModuleManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public abstract class CommonsPlugin extends JavaPlugin {
+public class CommonsPlugin extends JavaPlugin {
 
     @Override
-    public void onEnable() { ModuleManager.load(getModuleClasses()); }
+    public void onEnable() {
+        enable();
+
+        /*
+        Loading modules
+         */
+        ModuleManager.load(getModuleClasses());
+    }
 
     @Override
-    public void onDisable() { ModuleManager.unload(getModuleClasses()); }
+    public void onLoad() {
+        load();
+    }
 
-    public abstract Class[] getModuleClasses();
+    @Override
+    public void onDisable() {
+        disable();
+
+        /*
+        Unloading modules
+         */
+        ModuleManager.unload(getModuleClasses());
+    }
+
+    /*
+    Abstract methods
+     */
+
+    public void enable() { };
+
+    public void load() { };
+
+    public void disable() { };
+
+    public Class[] getModuleClasses() { return null; }
 
 }
